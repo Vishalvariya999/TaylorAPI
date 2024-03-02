9d5457b28c629d3458e44c79c474f0e42c595932
Vishal Variya <vishalvariya999@gmail.com>
2024-03-02 10:09:38 +0530

    Add data base connection

--- TaylorAPI/Program.cs
+++ TaylorAPI/Program.cs
@@ -1,6 +1,14 @@
+using Domain.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<TaylorDBContext>(options =>
+{
+    var connectionString = builder.Configuration.GetConnectionString(nameof(TaylorDBContext));
+    options.UseSqlServer(connectionString);
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
